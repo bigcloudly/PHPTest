8ba2c65eddbcd4e3091485ca84839de59005cbf8
cloudly <cloudly@cloudly.tw>
2018-02-02 13:59:23 +0800

    Ch4_2_1

---
+++ Ch4_2_1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="zh-TW ">
+<head>
+    <meta charset="utf-8"/>
+    <title>Ch4_2_1</title>
+</head>
+<body>
+<?php
+$animal[] = "dog";
+$animal[] = "cat";
+$animal[] = "tiger";
+$grades = array(78, 55, 89, 93);
+print_r($animal);
+echo "</br>";
+var_dump($grades);
+?>
+</body>
+</html>
