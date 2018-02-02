1bb9a1312f2db9ab9c4dee2bc7c944d3c48d9699
cloudly <cloudly@cloudly.tw>
2018-02-02 13:11:26 +0800

    2_2_1

---
+++ Ch2_2_1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="zh-TW ">
+<head>
+    <meta charset="utf-8"/>
+    <title>Ch2_2_1.php</title>
+</head>
+<body>
+<?php
+	$name = "測試1";
+	$age = 35;
+	$id = "1234";
+	print "姓名： " . $name. "<br/>";
+	print "編號： " . $id . "<br/>";
+	print "年齡： " . $age . "<br/>";
+?>
+</body>
+</html>
