8b87eb69eeae3f72196f62735c70744e26ac9b51
cloudly <cloudly@cloudly.tw>
2018-02-26 11:47:49 +0800

    echoTest

--- echoTest.php
+++ echoTest.php
@@ -1,7 +1,7 @@
 <?php
 echo "Hello World<br/>";
 $stamps = time();
-echo $stamps, "<br/>";
+echo "現在距離1970年01月01日共計", $stamps, "秒<br/>";
 $today = getdate();
 $year = $today["year"];
 $month = $today["mon"];
