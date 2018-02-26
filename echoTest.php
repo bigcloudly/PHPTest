2aebb083ed2786afb7f644034e76a471fb9f9373
cloudly <cloudly@cloudly.tw>
2018-02-26 10:41:49 +0800

    echoTest

--- echoTest.php
+++ echoTest.php
@@ -1,4 +1,14 @@
 <?php
-      echo "Hello World";
-      echo date(Ymd);
+echo "Hello World<br/>";
+$stamps = time();
+echo $stamps, "<br/>";
+$today = getdate();
+$year = $today["year"];
+$month = $today["mon"];
+$day = $today["mday"];
+$hour = $today["hours"];
+$minute = $today["minutes"];
+$second = $today["seconds"];
+
+echo $year, "年", $month, "月", $day, "日", $hour, "時", $minute, "分", $second, "秒";
 ?>
