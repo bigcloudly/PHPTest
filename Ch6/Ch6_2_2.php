57b3e069445f62a6ce6f3593239fb26090cb0bd6
cloudly <cloudly@cloudly.tw>
2018-03-02 13:37:19 +0800

    Ch6

---
+++ Ch6/Ch6_2_2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="zh-TW ">
+<head>
+    <meta charset="utf-8"/>
+    <title>Ch6_2_2</title>
+</head>
+<body>
+<form method = "post" action = "Ch6_2_2.php">
+	<p>回應：
+	<textarea name = "comment" id = "comment" rows = "5" cols = "30">
+	</textarea></p>
+	<input type = "submit" name = "send" value = "送出" />
+</form><hr/>
+<?php
+//$result = "";
+//$result = "";
+if (isset($_POST["send"])) {
+	$resust = $_POST["comments"];
+	echo "留言1：" . $result;
+	$result = nl2br($result);
+	echo "<hr/>留言2：" . $result;
+}
+?>
+</body>
+</html>
